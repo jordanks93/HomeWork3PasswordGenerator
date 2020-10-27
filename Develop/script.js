d88e4ec5356eec369f1e97e5db2a43e05fb14a7d
Jordanks93 <69221166+jordanks93@users.noreply.github.com>
2020-10-27 16:06:15 -0400

    added generatePassword function, not working yet

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  var letters = "abcdefghijklmnopqrstuvwxyz";
+  var numbers = [0,1,2,4,5,6,7,8,9];
+  var specials = 
+
+  
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +20,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+console.log(letters[1]);
