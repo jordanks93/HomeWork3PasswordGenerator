382325c0c6f56270c271a1aa83d7516b6d35c3cb
Jordanks93 <69221166+jordanks93@users.noreply.github.com>
2020-10-27 21:32:58 -0400

    updated generate password function with prompts and checks for correct user input

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,43 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword() {
-  var letters = "abcdefghijklmnopqrstuvwxyz";
-  var numbers = [0,1,2,4,5,6,7,8,9];
-  var specials = 
+// variables for password generator
+var letters = "abcdefghijklmnopqrstuvwxyz";
+var numbers = [0, 1, 2, 4, 5, 6, 7, 8, 9];
+var specials = ["\u0020", "\u0021", "\u0022", "\u0023", "\u0024", "\u0025", "\u0026", "\u0027",
+  "\u0028", "\u0029", "\u002A", "\u002B", "\u002C", "\u002D", "\u002E", "\u002F", "\u003A", "\u003B",
+  "\u003C", "\u003D", "\u003E", "\u003F", "\u0040", "\u005B", "\u005C", "\u005D", "\u005E", "\u005F"
+  , "\u0060", "\u007B", "\u007C", "\u007D", "\u007E"];
 
-  
+// Prompt the customer for password criteria
+  //length between 8-124 characters
+  //character types: lowercase, uppercase, numeric, and/or special characters
+  //atleast one character type should be selected, else prompt again
+
+function generatePassword() {
+  var atleastOne = true; // validates if user picked atleast one character type
+  while(atleastOne === true) {
+    var howLong = prompt("How long do you want your password to be? (between 8-128 characters)");
+    // makes sure input is between 8-128 before continuing 
+    if(howLong >= 8 && howLong <= 128) { 
+      var confirmUCLetters = confirm("Do you want uppercase letters in your password?");
+      var confirmLCLetters = confirm("Do you want lowercase letters in your password?");
+      var confirmNumbers = confirm("Do you want numbers in your password?");
+      var confirmSpecial = confirm("Do you want special characters in your password?");
+    }
+    else {
+      alert("You must choose a password length between 8 and 128 characters. Click generate password to retry.");
+      atleastOne = false
+    }
+    
+    if(confirmUCLetters === false && confirmLCLetters === false && confirmNumbers === false && confirmSpecial === false) {
+      console.log("You need to pick one atleast");
+      atleastOne = false;
+    }
+    
+    atleastOne = false;
+    
+  } 
 }
 
 // Write password to the #password input
@@ -21,4 +52,6 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-console.log(letters[1]);
+for(i=0; i< specials.length; i++) {
+  console.log(specials[i]);
+}
